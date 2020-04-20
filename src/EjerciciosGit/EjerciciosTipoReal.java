235d165c0af5c9607fe9b527ca328ba18ff392e7
claudia <cxtunubala@misena.edu.co>
2020-04-19 19:57:12 -0500

    Se crea la estructura del proyecto que contedra los ejercicios propuestos por el profesor: VERSION 1.0

---
+++ src/EjerciciosGit/EjerciciosTipoReal.java
@@ -0,0 +1,17 @@
+package EjerciciosGit;
+
+import java.util.Scanner;
+
+public class EjerciciosTipoReal 
+{
+	//1 Realizar un ejercicio en donde se pida un dato de tipo
+	//real(decimal) y lo divida entre 100, 
+	//la respuesta debe ser de tipo float.
+
+	public static void main(String[] args)
+	{
+		
+		
+	}
+
+}
---
+++ src/EjerciciosGit/SintomasCovid19.java
@@ -0,0 +1,23 @@
+package EjerciciosGit;
+
+import java.util.Scanner;
+
+public class SintomasCovid19 
+{
+	//2 Realizar un ejercicio en donde se muestre un menú que diga
+	//ingreso a la cámara detectora del segundo síntoma del covid-19.
+	//Por favor ingrese su temperatura, 
+	//luego de ingresar la temperatura por teclado 
+	//si esa temperatura es mayor a 37, imprima el mensaje 
+	//" necesitas chequeo de un médico, pues tienes
+	//uno de los síntomas de virus,"
+	//" si su temperatura es menor a 37 imprima cuidate
+
+	public static void main(String[] args) {
+
+		
+		
+		
+	}
+
+}
